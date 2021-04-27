1432e0e152d4d57fea7530141056a8e5daee55f5
Theendringer <82174986+Theendringer@users.noreply.github.com>
2021-04-27 15:43:14 -0300

    Completed module

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -15,11 +15,4 @@ public class App
         return 2 * number;
     }
 
-    private static int add(int[] numbers) {
-        var sum = 0;
-        for(int number: numbers ) {
-            sum =+ number;
-        }
-        return sum;
-    }
 }
